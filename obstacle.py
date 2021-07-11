6cb0d6ac68c721ce89887561aaaf365a1477da34
skrgich <dadoskrgic@gmail.com>
2021-07-11 09:36:30 +0200

    added obstacle.py and class obstacle

---
+++ obstacle.py
@@ -0,0 +1,37 @@
+from imgs import OBSTACLE_IMG
+import pygame
+
+class Obstacle:
+    SPEED = 5
+    IMG = OBSTACLE_IMG
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.y
+        self.tilt = 0
+        self.side = True
+
+    def draw(self, win):
+        rotated_image = pygame.transform.rotate(self.img, self.tilt * self.side)
+        new_rect = rotated_image.get_rect(center = self.img.get_rect(topleft = (self.x, self.y)).center)
+        win.blit(rotated_image, new_rect.topleft)
+
+    def move(self):
+        if self.side:
+            if self.tilt < 45:
+                self.tilt += 5
+            else:
+                self.side = not self.side
+                self.tilt -= 5
+                self.IMG = pygame.transform.flip(self.IMG, True, False)
+        else:
+            if self.tilt > -45:
+                self.tilt -= 5
+            else:
+                self.side = not self.side
+                self.tilt += 5
+                self.IMG = pygame.transform.flip(self.IMG, True, False)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)

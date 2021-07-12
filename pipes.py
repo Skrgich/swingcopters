9fb195474412868b340f5b27c209d4f36a4a3d25
skrgich <dadoskrgic@gmail.com>
2021-07-12 23:28:00 +0200

    finished pipe class

--- pipes.py
+++ pipes.py
@@ -8,7 +8,12 @@ class Pipe:
     def __init__(self, y):
         self.y = y
         self.dist = 0
-        self.end
+        self.left = 0
+        self.right = 0
+        self.PIPE_RIGHT = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_LEFT = PIPE_IMG
+        self.passed = False
+        self.set_dist()
 
     def set_dist(self):
         self.dist = random.randrange(40,450)
@@ -17,4 +22,22 @@ class Pipe:
 
     def move(self):
         self.y += self.SPEED
-        
+
+    def draw(self, win):
+        win.blit(self.PIPE_LEFT,(self.y, self.left))
+        win.blit(self.PIPE_LEFT, (self.y, self.right))
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        left_mask = pygame.mask.from_surface(self.PIPE_LEFT)
+        right_mask = pygame.mask.from_surface(self.PIPE_RIGHT)
+
+        left_offset = (self.left - bird.x, self.y - round(bird.y))
+        right_offset = (self.right - bird.x, self.y - round(bird.y))
+
+        left_point = bird_mask.overlap(left_mask, left_offset)
+        right_point = bird_mask.overlap(right_mask, right_offset)
+
+        if left_point or right_point:
+            return True
+        return False

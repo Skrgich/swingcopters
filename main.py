9e3235234ed7535e781e07153175d8c6ec9936ad
skrgich <dadoskrgic@gmail.com>
2021-09-08 00:16:58 +0200

    fixed neat inputs and curr pipe's index

--- main.py
+++ main.py
@@ -64,30 +64,40 @@ def main(genomes, config):
     clock = pygame.time.Clock()
     run = True
     score = 0
+    pipe_ind = 0
     while run:
-        clock.tick(30)
+        clock.tick(1000)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
                 quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    for pilot in pilot_list:
-                        pilot.turn()
+        #     if event.type == pygame.KEYDOWN:
+        #         if event.key == pygame.K_SPACE:
+        #             for pilot in pilot_list:
+        #                 pilot.turn()
         
-        pipe_ind = 0
-        if len(pilot_list) > 0:
-            if len(pipes_list) > 1 and pilot_list[0].y < pipes_list[0].y + pipes_list[0].PIPE_LEFT.get_width():
-                pipe_ind = 1
-        else:
+        # pipe_ind = 0
+        # if len(pilot_list) > 0:
+        #     if len(pipes_list) > 1 and pilot_list[0].y < pipes_list[0].y + pipes_list[0].PIPE_LEFT.get_height() // 2:
+        #         pipe_ind = 1
+        #         if len(pipes_list) > 2 and pilot_list[0].y < pipes_list[1].y + pipes_list[1].PIPE_LEFT.get_height() // 2:
+        #             pipe_ind = 2
+                
+        # else:
+        if not pilot_list:
             run = False
             break
-    
+        print(pipe_ind)
         for index, pilot in enumerate(pilot_list):
             pilot.move()
             ge[index].fitness += 1
-            output = nets[index].activate((pilot.y, abs(pilot.x - pipes_list[pipe_ind].left), abs(pilot.left - pipes_list[pipe_ind].right)))
+            output = nets[index].activate((pilot.y, 
+            abs(pilot.x - pipes_list[pipe_ind].left), 
+            abs(pilot.x - pipes_list[pipe_ind].right),
+            pipes_list[pipe_ind].left_obstacle.tilt,
+            pipes_list[pipe_ind].right_obstacle.tilt,
+            pilot.side))
 
             if output[0] > 0.5:
                 pilot.turn()
@@ -108,8 +118,9 @@ def main(genomes, config):
                 if not pipe.passed and pipe.y > pilot.y:
                     pipe.passed = True
                     add_pipe = True
+                    pipe_ind += 1
             
-            if pipe.y + pipe.PIPE_LEFT.get_height() < 0:
+            if pipe.y >= 800:# pipe.PIPE_LEFT.get_height() + 800:
                 rem.append(pipe)
             
             pipe.move()
@@ -123,6 +134,7 @@ def main(genomes, config):
         
         for p in rem:
             pipes_list.remove(p)
+            pipe_ind -= 1
         
         for i, pilot in enumerate(pilot_list):
             if pilot.y + pilot.img.get_height() >= 730 or pilot.y < 0:
@@ -133,5 +145,17 @@ def main(genomes, config):
 
         draw_window(win, pilot_list, pipes_list, base_list, score, gen)
 
+def run(config_path):
+    config=neat.config.Config(neat.DefaultGenome,neat.DefaultReproduction,neat.DefaultSpeciesSet,neat.DefaultStagnation,config_path)
 
-main()
+    p=neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats=neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner=p.run(main)
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward.txt')
+    run(config_path)

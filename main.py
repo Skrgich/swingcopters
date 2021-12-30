598b1fa0511360175d9b31b7639558af3512b958
skrgich <dadoskrgic@gmail.com>
2021-12-31 00:14:18 +0100

    cleaned part that's not working

--- main.py
+++ main.py
@@ -73,36 +73,15 @@ def main(genomes, config):
                 run = False
                 pygame.quit()
                 quit()
-        #     if event.type == pygame.KEYDOWN:
-        #         if event.key == pygame.K_SPACE:
-        #             for pilot in pilot_list:
-        #                 pilot.turn()
-        
-        # pipe_ind = 0
-        # if len(pilot_list) > 0:
-        #     if len(pipes_list) > 1 and pilot_list[0].y < pipes_list[0].y + pipes_list[0].PIPE_LEFT.get_height() // 2:
-        #         pipe_ind = 1
-        #         if len(pipes_list) > 2 and pilot_list[0].y < pipes_list[1].y + pipes_list[1].PIPE_LEFT.get_height() // 2:
-        #             pipe_ind = 2
-                
-        # else:
+
         if not pilot_list:
             run = False
             break
-        # print(pipe_ind)
+
         for index, pilot in enumerate(pilot_list):
             pilot.move()
             ge[index].fitness += 1
-            # if not pilot.side:
-            #      output = nets[index].activate((pilot.x, 
-            # pilot.x - pipes_list[pipe_ind].right,
-            # pipes_list[pipe_ind].right_obstacle.new_rect.topleft[0],
-            # pipes_list[pipe_ind].right_obstacle.new_rect.topleft[1]))
-            # else:
-            #      output = nets[index].activate((pilot.x, 
-            # -pilot.x + pipes_list[pipe_ind].left, 
-            # pipes_list[pipe_ind].left_obstacle.new_rect.topleft[0],
-            # pipes_list[pipe_ind].left_obstacle.new_rect.topleft[1]))
+
             t = (t + 1) % 36
             output = nets[index].activate((pilot.y - pipes_list[pipe_ind].y, 
                 (pilot.x - pipes_list[pipe_ind].left), 
@@ -114,35 +93,12 @@ def main(genomes, config):
                 (pilot.side - 0.5) * 2
                 ))
 
-            # if pilot.side:
-            #     output = nets[index].activate((pilot.y - pipes_list[pipe_ind].y, 
-            #     (pilot.x - pipes_list[pipe_ind].left), 
-            #     (pilot.x - pipes_list[pipe_ind].right),
-            #     (pilot.x - pipes_list[pipe_ind].left_obstacle.new_rect.bottomright[0]),
-            #     (pilot.y - pipes_list[pipe_ind].left_obstacle.new_rect.bottomright[1]),
-            #     (pilot.x - pipes_list[pipe_ind].right_obstacle.new_rect.bottomleft[0]),
-            #     (pilot.y - pipes_list[pipe_ind].right_obstacle.new_rect.bottomleft[1]),
-            #     t
-            #     ))
-            # else:
-            #     output = nets[index].activate((pilot.y - pipes_list[pipe_ind].y, 
-            #     (pilot.x - pipes_list[pipe_ind].right),
-            #     (pilot.x - pipes_list[pipe_ind].left), 
-            #     (pilot.x - pipes_list[pipe_ind].right_obstacle.new_rect.bottomleft[0]),
-            #     (pilot.y - pipes_list[pipe_ind].right_obstacle.new_rect.bottomleft[1]),
-            #     (pilot.x - pipes_list[pipe_ind].left_obstacle.new_rect.bottomright[0]),
-            #     (pilot.y - pipes_list[pipe_ind].left_obstacle.new_rect.bottomright[1]),
-            #     t
-            #     ))     
-        
             if output[0] < 0:
                 if pilot.side:
                     pilot.turn()
             else:
                 if not pilot.side:
                     pilot.turn()
-
-            #pilot.turn()
 
         base_list.move()
         rem = []
@@ -160,7 +116,7 @@ def main(genomes, config):
                     add_pipe = True
                     pipe_ind += 1
             
-            if pipe.y >= 800:# pipe.PIPE_LEFT.get_height() + 800:
+            if pipe.y >= 800:
                 rem.append(pipe)
             
             pipe.move()
